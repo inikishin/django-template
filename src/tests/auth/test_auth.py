ab594a74738bee891e73f914abc8a7d2113f3fd3
darkstorn <inikishin@gmail.com>
2023-07-26 15:24:10 +0300

    add test and linters

---
+++ src/tests/auth/test_auth.py
@@ -0,0 +1,39 @@
+import pytest
+from rest_framework import status
+
+
+@pytest.mark.django_db
+def test_login(loaddata, api_client):
+    url = '/auth/login/'
+    data = {
+        'username': 'admin',
+        'password': 'admin'
+    }
+    response = api_client.post(url, data=data)
+
+    assert response.status_code == status.HTTP_200_OK
+
+    content = response.data
+    assert "access" in content
+    assert "refresh" in content
+
+
+@pytest.mark.django_db
+def test_refresh(loaddata, api_client):
+    url = '/auth/login/'
+    data = {
+        'username': 'admin',
+        'password': 'admin'
+    }
+    response = api_client.post(url, data=data)
+    assert response.status_code == status.HTTP_200_OK
+    refresh_token = response.data.get('refresh')
+
+    url = '/auth/refresh/'
+    data = {
+        'refresh': refresh_token,
+    }
+    response = api_client.post(url, data=data)
+    assert response.status_code == status.HTTP_200_OK
+    content = response.data
+    assert "access" in content
---
+++ src/tests/conftest.py
@@ -0,0 +1,29 @@
+import pytest
+from pathlib import Path
+
+from django.core.management import call_command
+from rest_framework.test import APIClient
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def loaddata():
+    dir_ = Path(__file__).resolve().parent
+    files = list(dir_.joinpath("data").glob("*.json"))
+    for file in files:
+        call_command("loaddata", file, verbosity=0)
+
+
+@pytest.fixture
+def api_client():
+    client = APIClient()
+    yield client
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def as_admin(api_client):
+    url = 'auth/login'
+    user, password = 'admin', 'admin'
+    response = api_client.post(url, data={"username": user, "password": password})
+
