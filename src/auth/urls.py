f2264b41305fba017c972e22469e88d269db00f6
darkstorn <inikishin@gmail.com>
2023-07-26 15:24:00 +0300

    add auth

---
+++ src/auth/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from auth.views import (
+    LoginView,
+    RefreshView,
+)
+
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('refresh/', RefreshView.as_view(), name='refresh'),
+]
