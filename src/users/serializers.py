0c595a3153ffe27fac3d6fe67aad3848e4e834ce
darkstorn <inikishin@gmail.com>
2023-09-14 11:22:17 +0300

    add user serializers

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -14,3 +14,28 @@ class UserSerializer(ModelSerializer):
             "date_joined",
             "is_superuser",
         ]
+
+
+class UserListSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "username",
+            "first_name",
+            "last_name",
+        ]
+
+
+class UserRetrieveSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "date_joined",
+            "is_superuser",
+        ]
