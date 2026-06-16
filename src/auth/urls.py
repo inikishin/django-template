7989bee828382928e8b389d9a2fabbe8e3d429d2
inikishin <inikishin@pandoraresearch.com>
2026-06-16 14:03:39 +0300

    update files

--- src/auth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from auth.views import LoginView, RefreshView
-
-urlpatterns = [
-    path("login/", LoginView.as_view(), name="login"),
-    path("refresh/", RefreshView.as_view(), name="refresh"),
-]
